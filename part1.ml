be05e71b1dc2b05312a56f230e39b8be04e8d13a
Nick De Tullio <nrd24@cornell.edu>
2013-03-27 03:03:03 -0400

    changed match statement to if then statements

--- part1.ml
+++ part1.ml
@@ -1,9 +1,7 @@
 let map (f : func) (t: tree) : (t :tree) -> 
 	let tsize = BinaryTree.size t in
 		let rec tree_cycle (func : func) (tree_index : int) (tr : tree) -> 
-			match tree_index with
-			| ((2*tree_index)+1) < tsize ->  tree_cycle func (2*tree_index+1) "final argument"
-			| ((2*tree_index)+2) < tsize ->  tree_cycle func (2*tree_index+2) "final argument"
-			| tree_index < tsize -> tree_cycle func tree_index "final argument"
-			| _ -> failwith "" in
+			if ((2*tree_index) + 1) < tsize then tree_cycle func ((2*tree_index) + 1) "final argument"
+			if ((2*tree_index) + 2) < tsize then tree_cycle func ((2*tree_index) + 2) "final argument"
+			if tree_index < tsize then tree_cycle func tree_index "final argument"
 	tree_cycle f 0 t
