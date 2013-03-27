6589f5988999a02024aba2e14138938c03ce4ec1
Nick De Tullio <nrd24@cornell.edu>
2013-03-27 12:00:45 -0400

    completed base case
    Elements will be removed, modified by the function and added back to
    the tree if they are members.

--- part1.ml
+++ part1.ml
@@ -1,7 +1,11 @@
-let map (f : func) (t: tree) : (t :tree) -> 
+let map f t =
 	let tsize = BinaryTree.size t in
-		let rec tree_cycle (func : func) (tree_index : int) (tr : tree) -> 
-			if ((2*tree_index) + 1) < tsize then tree_cycle func ((2*tree_index) + 1) "final argument"
-			if ((2*tree_index) + 2) < tsize then tree_cycle func ((2*tree_index) + 2) "final argument"
-			if tree_index < tsize then tree_cycle func tree_index "final argument"
+		let rec tree_cycle func tree_index tr = 
+			if (tree_index < tsize) && (member tr tree_index) 
+        then let new_tree = remove tr tree_index && add tr (f tree_index) in
+      if ((2*tree_index + 1) < tsize) && (member tr tree_index) 
+        then if ((2*tree_index + 2) < tsize) && (member tr tree_index)
+               then tree_cycle func (2*tree_index + 2) (tree_cycle func (2*tree_index + 1) new_tree)
+             else tree_cycle func (2*tree_index + 1) new_tree
+      else new_tree in
 	tree_cycle f 0 t
