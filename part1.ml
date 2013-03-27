9fa86b1c12841cce89f666e6e7b1c7a0a7aa95d4
Nick De Tullio <nrd24@cornell.edu>
2013-03-27 19:05:01 -0400

    added calls to the BinaryTree module for add and remove

--- part1.ml
+++ part1.ml
@@ -1,10 +1,10 @@
 let map f t =
 	let tsize = BinaryTree.size t in
 		let rec tree_cycle func tree_index tr = 
-			if (tree_index < tsize) && (member tr tree_index) 
-        then let new_tree = remove tr tree_index && add tr (f tree_index) in
-      if ((2*tree_index + 1) < tsize) && (member tr tree_index) 
-        then if ((2*tree_index + 2) < tsize) && (member tr tree_index)
+			if (tree_index < tsize) && (BinaryTree.member tr tree_index) 
+        then let new_tree = BinaryTree.remove tr tree_index && BinaryTree.add tr (f tree_index) in
+      if ((2*tree_index + 1) < tsize) && (BinaryTree.member tr tree_index) 
+        then if ((2*tree_index + 2) < tsize) && (BinaryTree.member tr tree_index)
                then tree_cycle func (2*tree_index + 2) (tree_cycle func (2*tree_index + 1) new_tree)
              else tree_cycle func (2*tree_index + 1) new_tree
       else new_tree in
