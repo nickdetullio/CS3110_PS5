5201525cdb088c03520d218e3a41a90cc7b7ace2
Nick De Tullio <nrd24@cornell.edu>
2013-03-27 19:40:34 -0400

    deleted unnecessary call to remove

--- part1.ml
+++ part1.ml
@@ -2,8 +2,7 @@ let map f t =
 	let tsize = BinaryTree.size t in
 		let rec tree_cycle func tree_index tr = 
 			if (tree_index < tsize) && (BinaryTree.member tr tree_index) 
-        then let new_tree = BinaryTree.remove tr tree_index 
-               && BinaryTree.add tr (f (*Value at tree_index*)) in
+        then let new_tree = BinaryTree.add tr (f (*Value at tree_index*)) in
       if ((2*tree_index + 1) < tsize) && (BinaryTree.member tr tree_index) 
         then if ((2*tree_index + 2) < tsize) && (BinaryTree.member tr tree_index)
                then tree_cycle func (2*tree_index + 2) (tree_cycle func (2*tree_index + 1) new_tree)
