230e51082b3ea308b46d196d00897f32b0cd1255
Nick De Tullio <nrd24@cornell.edu>
2013-03-30 23:05:55 -0400

    completed and tested hashtable

--- shared/hashtable.ml
+++ shared/hashtable.ml
@@ -1,19 +1,47 @@
-type ('a, 'b) t
+type ('a, 'b) t = ('a * 'b) list array ref * int ref * ('a -> int)
 
-let create capacity hash = 
-  failwith "Die monster. You don't belong in this world!"
+let create capacity hash : ('a, 'b) t = 
+  (ref (Array.make capacity []), ref 0, fun x -> (hash x) mod capacity)
+  
+let iter f ((a, i, _) : ('a, 'b) t) = 
+  Array.iter (List.iter (fun (key, value) -> f key value)) !a
 
-let add table key value = 
-  failwith "It was not by my hand that I am once again given flesh. I was called here by humans, who wish to pay me tribute."
+let rec table_double (a, i, f)  = 
+  let size = Array.length !a in
+  if !i >= ((3 * size) / 4) then
+    let (a2, i, f) = create (2 * size) f in
+      iter (fun key value -> add (a2, i, f) key value) (a, i, f);
+      a := !a2
+  else ()
 
-let find table key = failwith "Tribute? You steal men's souls, and make them your slaves."
+and add ((a, i, f) : ('a, 'b) t) key value =  
+  table_double (a, i, f);
+  let lst = Array.get !a (f key) in 
+  if List.mem_assoc key lst then
+    Array.set !a (f key) ((key, value) :: (List.remove_assoc key lst))  
+  else 
+    (Array.set !a (f key) ((key, value) :: lst); 
+     i := !i + 1)
+ 
+let find ((a, i, f) : ('a, 'b) t) key = 
+  let lst = Array.get !a (f key) in
+  List.assoc key lst
 
-let mem table key = failwith "Perhaps the same could be said of all religions."
+let mem ((a, i, f) : ('a, 'b) t) key = 
+  let lst = Array.get !a (f key) in
+  List.mem_assoc key lst
 
-let remove table key = failwith "Your words are as empty as your soul. Mankind ill needs a savior such as you."
-
-let iter f table = failwith "What is a man?! A miserable little pile of secrets! But enough talk, have at you!"
-
-let fold f table init = failwith "(The battle rages)"
-
-let length table = failwith "No! This cannot be! AAAAAAAAAH!!!"
+let remove ((a, i, f) : ('a, 'b) t) key = 
+  let lst = Array.get !a (f key) in
+  if List.mem_assoc key lst then
+    (Array.set !a (f key) (List.remove_assoc key lst); i := !i - 1)
+  else ()
+  
+let fold f (table : ('a, 'b) t) init = 
+  let acc_ref = ref init in
+  let helper key value = 
+    acc_ref := f key value !acc_ref in
+  iter helper table;
+  !acc_ref 
+  
+let length ((a, i, f) : ('a, 'b) t) = !i 
