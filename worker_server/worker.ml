a16e734ede8b932b75afa396639248a02ee23acf
Nick De Tullio <nrd24@cornell.edu>
2013-04-05 04:05:49 -0400

    completed handle_request

--- worker_server/worker.ml
+++ worker_server/worker.ml
@@ -8,21 +8,64 @@ let send_response client response =
     else ());
     success
 
+let mappers = ref []
+
+let reducers = ref []
+
 let rec handle_request client =
   match Connection.input client with
-    Some v ->
+    Some v -> 
       begin
         match v with
         | InitMapper source -> 
-          failwith "It's been a long time, old one."
+            let program = Program.build source in
+            match program with
+            | (None, error) -> if send_response client (Mapper (None, error))
+                                 then handle_request client
+                               else failwith "InitMapper failed"
+            | (Some id, str) -> mappers := id :: !mappers;
+                               if send_response client (Mapper (Some id, str)) 
+                                 then handle_request client
+                               else failwith "InitMapper failed"
         | InitReducer source -> 
-          failwith "Young master, I cannot aid one who opposes the Master!"
+            let program = Program.build source in
+              match program with
+              | (None, error) -> if send_response client (Reducer (None, error))
+                                   then handle_request client
+                                 else failwith "InitMapper failed"
+              | (Some id, str) -> reducers := id :: !reducers;
+                                 if send_response client (Reducer (Some id, str)) 
+                                   then handle_request client
+                                 else failwith "InitMapper failed"
         | MapRequest (id, k, v) -> 
-          failwith "You won't go unrewarded."
+            if not (List.mem !mappers id) then 
+                               begin
+                                 if send_response client (InvalidWorker id)
+                                   then handle_request client
+                                 else failwith "MapRequest failed"
+                               end
+            else match Program.run id v with
+                 | None -> if send_response client (RuntimeError (id, v)) 
+                             then handle_request client
+                           else failwith "MapRequest failed"
+                 | Some result -> if send_response client (MapResults id, results)
+                                    then handle_request client
+                                  else failwith "MapRequest failed"    
         | ReduceRequest (id, k, v) -> 
-          failwith "Really? In that case, just tell me what you need."
+            if not (List.mem !reducers id) then 
+                       begin
+                         if send_response client (InvalidWorker id)
+                           then handle_request client
+                         else failwith "ReduceRequest failed"
+                       end
+            else match Program.run id v with
+                 | None -> if send_response client (RuntimeError (id, v)) 
+                             then handle_request client
+                           else failwith "ReduceRequest failed"
+                 | Some result -> if send_response client (ReduceResults id, results)
+                                    then handle_request client
+                                  else failwith "ReduceRequest failed" 
       end
   | None ->
       Connection.close client;
       print_endline "Connection lost while waiting for request."
-
