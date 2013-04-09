8929e1a4ef10e51954f31bf0a30fe48001d6c230
Nick De Tullio <nrd24@cornell.edu>
2013-04-09 17:27:42 -0400

    reduced length of lines to > 80 chars

--- apps/inverted_index/mapper.ml
+++ apps/inverted_index/mapper.ml
@@ -1,3 +1,3 @@
 let (key, value) = Program.get_input() in
-Program.set_output (List.fold_left (fun acc k -> (k, string_of_int key)::acc) []
-                   (List.map String.lowercase (Util.split_words value)))
+Program.set_output (List.fold_left (fun acc k -> (k, string_of_int key)::acc) 
+  [] (List.map String.lowercase (Util.split_words value)))
--- apps/inverted_index/reducer.ml
+++ apps/inverted_index/reducer.ml
@@ -1,3 +1,4 @@
 let (key, values) = Program.get_input() in
-let reduced_list = List.fold_left (fun acc v -> if List.mem values v then acc else v::acc) 0 values in
-    Program.set_output [reduced_list]
+let reduced_list = List.fold_left 
+  (fun acc v -> if List.mem values v then acc else v::acc) 0 values in
+Program.set_output [reduced_list]
