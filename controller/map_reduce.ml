456caf98511c48589befb9f51a703f13cd93a2be
Nick De Tullio <nrd24@cornell.edu>
2013-04-08 23:59:44 -0400

    implemented reduce

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -13,7 +13,7 @@ let map kv_pairs map_filename : (string * string) list =
     let helper = 
 	    let mapper = Worker_manager.pop_worker work_man in
 		Mutex.lock queue_lock;
-	    let (k, v) = Queue.push work_queue in 
+	    let (k, v) = Queue.pop work_queue in 
 		Mutex.unlock queue_lock;
 	      match Worker_manager.map mapper k v with 
 	      | [Some l] -> Mutex.lock output_lock
@@ -30,7 +30,30 @@ let combine kv_pairs : (string * string list) list =
   failwith "You have been doomed ever since you lost the ability to love."
   
 let reduce kvs_pairs reduce_filename : (string * string list) list =
-  failwith "The only thing necessary for evil to triumph is for good men to do nothing"
+  let work_queue = Queue.create in
+  List.iter (Queue.push work_queue) reduce_filename;
+  let thread_pool = thread_pool.create in 
+  let work_man = Worker_manager.intialize_reducers map_filename in
+  let output_list = ref [] in
+  let queue_lock = Mutex.create() in
+  let output_lock = Mutex.create() in
+  while not (Queue.is_empty work_queue) do
+    let helper = 
+	    let reducer = Worker_manager.pop_worker work_man in
+		Mutex.lock queue_lock;
+    let string_list = ref [] in
+	    let (k, v_list) = Queue.pop work_queue in
+		Mutex.unlock queue_lock;
+	      match Worker_manager.reduce reducer k v_list with 
+	      | [Some l] -> Mutex.lock output_lock
+						output_list := List.append l !output_list;
+						Mutex.unlock output_lock;
+						Worker_manager.push_worker work_man reducer
+	      | [None] -> Queue.push (k, v_list) in
+    Thread_pool.add_work helper thread_pool;
+  done;
+  Worker_manager.clean_up_workers
+  !output_list
 
 let map_reduce app_name mapper_name reducer_name kv_pairs =
   let mapper = Printf.sprintf "apps/%s/%s.ml" app_name mapper_name
