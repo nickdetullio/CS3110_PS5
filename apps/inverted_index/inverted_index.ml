84a4ef75c8813b9ff59663a0c96ab96127c04d71
Chris Cassano <cac366@cornell.edu>
2013-04-09 16:19:21 -0400

    Actually committed inverted_index
    Ignore previous commit for inverted_index

--- apps/inverted_index/inverted_index.ml
+++ apps/inverted_index/inverted_index.ml
@@ -1,9 +1,15 @@
 open Util;;
+open Map_reduce;;
 
 let main (args : string array) : unit = 
   if Array.length args < 3 then 
     Printf.printf "Usage: inverted_index <filename>"
   else
-    failwith "What need for the shepherd when the wolves have all gone?"
+    let filename = args.(2) in
+    let docs = load_documents filename in
+    let kv_pairs = List.rev_map (fun d -> (string_of_int d.id, d.body)) docs in
+    let reduced = 
+      Map_reduce.map_reduce "inverted_index" "mapper" "reducer" kv_pairs in
+    print_reduced_documents reduced 
 in
 main Sys.argv
