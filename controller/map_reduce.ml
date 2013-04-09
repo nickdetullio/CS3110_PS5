968bf2d5cff9d06559781c538b7ab36916b67621
Nick De Tullio <nrd24@cornell.edu>
2013-04-09 03:34:52 -0400

    compiled worker.ml and map / combine in map_reduce.ml

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -2,28 +2,28 @@ open Util
 open Worker_manager
 
 let map kv_pairs map_filename : (string * string) list = 
-  let work_queue = Queue.create in
+  let work_queue = Queue.create () in
   List.iter (Queue.push work_queue) kv_pairs;
-  let thread_pool = Thread_pool.create in 
-  let work_man = Worker_manager.intialize_mappers map_filename in
+  let thread_pool = Thread_pool.create 100 in 
+  let work_man = Worker_manager.initialize_mappers map_filename in
   let output_list = ref [] in
   let queue_lock = Mutex.create() in
   let output_lock = Mutex.create() in
   while not (Queue.is_empty work_queue) do
-    let helper = 
+    let helper () = 
 	    let mapper = Worker_manager.pop_worker work_man in
 		Mutex.lock queue_lock;
 	    let (k, v) = Queue.pop work_queue in 
 		Mutex.unlock queue_lock;
 	      match Worker_manager.map mapper k v with 
-	      | [Some l] -> Mutex.lock output_lock
+	      | Some l -> Mutex.lock output_lock;
 						output_list := List.append l !output_list;
 						Mutex.unlock output_lock;
-						Worker_manager.push_worker work_man mapper
-	      | [None] -> Queue.push (k, v) in
+						Worker_manager.push_worker work_man mapper;
+	      | None -> Queue.push (k, v) work_queue; in
     Thread_pool.add_work helper thread_pool;
   done;
-  Worker_manager.clean_up_workers
+  Worker_manager.clean_up_workers;
   !output_list
   
 let combine kv_pairs : (string * string list) list = 
@@ -39,29 +39,28 @@ let combine kv_pairs : (string * string list) list =
   Hashtbl.fold (fun k (k, v_list) acc -> (k, v_list) :: acc) hash_table []
   
 let reduce kvs_pairs reduce_filename : (string * string list) list =
-  let work_queue = Queue.create in
-  List.iter (Queue.push work_queue) reduce_filename;
-  let thread_pool = Thread_pool.create in 
-  let work_man = Worker_manager.intialize_reducers map_filename in
+  let work_queue = Queue.create () in
+  List.iter (Queue.push work_queue) kvs_pairs;
+  let thread_pool = Thread_pool.create 100 in 
+  let work_man = Worker_manager.initialize_reducers reduce_filename in
   let output_list = ref [] in
   let queue_lock = Mutex.create() in
   let output_lock = Mutex.create() in
   while not (Queue.is_empty work_queue) do
-    let helper = 
+    let helper () = 
 	    let reducer = Worker_manager.pop_worker work_man in
 		Mutex.lock queue_lock;
-    let string_list = ref [] in
 	    let (k, v_list) = Queue.pop work_queue in
 		Mutex.unlock queue_lock;
 	      match Worker_manager.reduce reducer k v_list with 
-	      | [Some l] -> Mutex.lock output_lock
-						output_list := List.append l !output_list;
+	      | Some l -> Mutex.lock output_lock;
+            output_list := List.append (k, l) !output_list;
 						Mutex.unlock output_lock;
-						Worker_manager.push_worker work_man reducer
-	      | [None] -> Queue.push (k, v_list) in
+						Worker_manager.push_worker work_man reducer;
+	      | None -> Queue.push (k, v_list) work_queue in
     Thread_pool.add_work helper thread_pool;
   done;
-  Worker_manager.clean_up_workers
+  Worker_manager.clean_up_workers;
   !output_list
 
 let map_reduce app_name mapper_name reducer_name kv_pairs =
--- worker_server/worker.ml
+++ worker_server/worker.ml
@@ -19,52 +19,52 @@ let rec handle_request client =
         match v with
         | InitMapper source -> 
             let program = Program.build source in
-            match program with
-            | (None, error) -> if send_response client (Mapper (None, error))
-                                 then handle_request client
-                               else failwith "InitMapper failed"
-            | (Some id, str) -> mappers := id :: !mappers;
-                               if send_response client (Mapper (Some id, str)) 
-                                 then handle_request client
-                               else failwith "InitMapper failed"
+            (match program with
+             | (None, error) -> if send_response client (Mapper (None, error))
+                                  then handle_request client
+                                else failwith "InitMapper failed"
+             | (Some id, str) -> mappers := id :: !mappers;
+                                 if send_response client (Mapper (Some id, str)) 
+                                   then handle_request client
+                                 else failwith "InitMapper failed")
         | InitReducer source -> 
             let program = Program.build source in
-              match program with
-              | (None, error) -> if send_response client (Reducer (None, error))
-                                   then handle_request client
-                                 else failwith "InitMapper failed"
-              | (Some id, str) -> reducers := id :: !reducers;
-                                 if send_response client (Reducer (Some id, str)) 
-                                   then handle_request client
-                                 else failwith "InitMapper failed"
+              (match program with
+               | (None, error) -> if send_response client (Reducer (None, error))
+                                    then handle_request client
+                                  else failwith "InitMapper failed"
+               | (Some id, str) -> reducers := id :: !reducers;
+                                   if send_response client (Reducer (Some id, str)) 
+                                    then handle_request client
+                                   else failwith "InitMapper failed")
         | MapRequest (id, k, v) -> 
-            if not (List.mem !mappers id) then 
-                               begin
-                                 if send_response client (InvalidWorker id)
-                                   then handle_request client
-                                 else failwith "MapRequest failed"
-                               end
-            else match Program.run id v with
-                 | None -> if send_response client (RuntimeError (id, v)) 
-                             then handle_request client
-                           else failwith "MapRequest failed"
-                 | Some result -> if send_response client (MapResults id, results)
-                                    then handle_request client
-                                  else failwith "MapRequest failed"    
+            (if not (List.mem id !mappers) then 
+                                 begin
+                                   if send_response client (InvalidWorker id)
+                                     then handle_request client
+                                   else failwith "MapRequest failed"
+                                 end
+             else match Program.run id v with
+                  | None -> if send_response client (RuntimeError (id, v)) 
+                                then handle_request client
+                            else failwith "MapRequest failed"
+                  | Some result -> if send_response client (MapResults (id, result))
+                                     then handle_request client
+                                   else failwith "MapRequest failed")
         | ReduceRequest (id, k, v) -> 
-            if not (List.mem !reducers id) then 
-                       begin
-                         if send_response client (InvalidWorker id)
-                           then handle_request client
-                         else failwith "ReduceRequest failed"
-                       end
-            else match Program.run id v with
-                 | None -> if send_response client (RuntimeError (id, v)) 
-                             then handle_request client
-                           else failwith "ReduceRequest failed"
-                 | Some result -> if send_response client (ReduceResults id, results)
-                                    then handle_request client
-                                  else failwith "ReduceRequest failed" 
+            (if not (List.mem id !reducers) then 
+                        begin
+                          if send_response client (InvalidWorker id)
+                            then handle_request client
+                          else failwith "ReduceRequest failed"
+                        end
+             else match Program.run id v with
+                  | None -> if send_response client (RuntimeError (id, List.hd v)) 
+                              then handle_request client
+                            else failwith "ReduceRequest failed"
+                  | Some result -> if send_response client (ReduceResults (id, result))
+                                     then handle_request client
+                                   else failwith "ReduceRequest failed") 
       end
   | None ->
       Connection.close client;
