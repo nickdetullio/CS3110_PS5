7491ff60fc54c67281ca4edace1098a2d2afad27
Chris Cassano <cac366@cornell.edu>
2013-04-11 21:45:04 -0400

    Implemented dna_sequence

--- apps/dna_sequencing/step_1_mapper.ml
+++ apps/dna_sequencing/step_1_mapper.ml
@@ -1,9 +1,13 @@
+let marshal_kvs = 
+  List.map (fun (k, v) -> (Util.marshal k, Util.marshal v)) in
+
 let (key, value) = Program.get_input() in
 let v : (string * int) = Util.unmarshal key in
 let (title, id) = v in
 let short_id = string_of_int id in
 let kmer_list = ref [] in 
-for i = 0 to ((String.length key) - 10) do
+for i = 0 to ((String.length value) - 10) do
 	let kmer_id = short_id ^ (string_of_int i) in 
-	kmer_list := (String.sub value i 10, (title, kmer_id, short_id, string_of_int i) :: !kmer_list;
-Program.set_output (kmer_list); 
+	kmer_list := (String.sub value i 10, (title, kmer_id, short_id, string_of_int i)) :: !kmer_list;
+done;
+Program.set_output (marshal_kvs !kmer_list); 
--- apps/dna_sequencing/step_2_reducer.ml
+++ apps/dna_sequencing/step_2_reducer.ml
@@ -1,5 +1,5 @@
 (* output = (short_id, (ref_id, short_index, ref_index)) *)
-(* (ref id, short_index, ref_index, seed_length) *)
+(* (short_index, seed_length, ref_id, ref_index) *)
 let (key, values) = Program.get_input() in
 	let helper ele1 ele2 = 
 		let (a, (b,first_index,d)) = ele1 in
@@ -7,14 +7,15 @@ let (key, values) = Program.get_input() in
 		if first_index = second_index then 0
 		else (if first_index > second_index then 1 else -1) in
 	let sorted_list = List.sort helper values in
-	let rec helper lst acc =
+	let rec helper lst acc curr_length =
 		match lst with 
 		| h1::h2::t -> let (t1, (t2,first_index,t3)) = h1 in
 				   let (t4, (t5,second_index,t6)) = h2 in
 				   if (second_index - first_index) < 10 then
-					 helper (::t  ::acc
+					 helper (t1, (t2,first_index,t3))::t acc (second_index + 10)
 				   else
-				   
+					 helper (t4, (t5, second_index, t6))::t (first_index, curr_length, t2, t3)::acc 0
+		| [] -> acc
 				   
 					
 				   
